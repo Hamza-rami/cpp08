bf90559b478655cf4d9020af7af0db1dcb5eaba5
Hamza Rami <ramihamza504@gmail.com>
2026-06-09 17:38:10 +0100

    start ex01

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,15 +1,33 @@
 #include "easyfind.hpp"
-#include <ostream>
+#include <iostream>
 #include <vector>
-
+#include <list>
+#include <deque>
 
 int main()
 {
 
-    std::vector<int> h = {1, 2, 3, 4};
+    std::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+
+    std::list<int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    l.push_back(4);
+
+
+    std::deque<int> d;
+    d.push_back(1);
+    d.push_back(2);
     try
     {
-        std::cout << easyfind(h, 4);
+        std::cout << easyfind(v, 4) << '\n';
+        std::cout << easyfind(l, 3) << '\n';
+        std::cout << easyfind(d, 4) << '\n';
     }
     catch(const std::exception& e)
     {
